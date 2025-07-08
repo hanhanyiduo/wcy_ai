dec69968c9ec624f0998da155076e8b9d28ddcef
hanhanyiduo <3227131845@qq.com>
2025-07-08 14:36:49 +0800

    react父子组件通信

---
+++ react/hooks-todos/src/components/Todos/TodoForm.jsx
@@ -0,0 +1,16 @@
+import { 
+    useState  // 私有状态  
+} from 'react'
+ const TodoForm = () => {
+    //JSX 一定得有唯一的最外层元素 树状结构 树来编译并解析JSX，
+    return (
+       <>
+        <h1 className='header'>TodoList</h1>
+        <form >
+ 
+        </form>
+       </>
+    )
+}
+
+export default TodoForm
---
+++ react/hooks-todos/src/components/Todos/TodoList.jsx
@@ -0,0 +1,10 @@
+import TodoItem from './TodoItem'
+const TodoList = () => {
+    return (
+        <>
+         
+         <TodoItem/>
+        </>
+    )
+}
+export default TodoList
