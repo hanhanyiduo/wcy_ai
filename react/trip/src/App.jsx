7fb42361fc75a0cf8c1df3f4c9d4320aec9167fa
hanhanyiduo <3227131845@qq.com>
2025-07-24 16:57:45 +0800

    移动端适配等项目初始

---
+++ react/trip/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div style={{
+        width: '2.66667rem',
+        height: '2.66667rem', 
+        background: 'red'}}></div>
+        <br />
+        <div style={{
+        width: '3.46667rem',
+        height: '5rem', 
+        background: 'red'}}></div>
+        <br />
+        <div className='box'></div>
+    </>
+  )
+}
+
+export default App
